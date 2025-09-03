2311f446ed1887844c3fec4e124818a5a6a553e8
Wildann133 <wildanmhfdz@gmail.com>
2025-09-03 14:51:56 +0700

    Complete tutorial 1: Django MVT implementation

--- football_news/asgi.py
+++ football_news/asgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 ASGI config for football_news project.
 
@@ -15,21 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'football_news.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for football_news project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'football_news.settings')
-
-application = get_asgi_application()
->>>>>>> d0b6b8d3e960d3b8ae1903c921a5bfc7ef134328
